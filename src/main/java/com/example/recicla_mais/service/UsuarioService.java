5cbf0c75e9e096eb83238772a714cf96fa235791
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-04-14 20:01:34 -0300

    feat/Adicionando Service

---
+++ src/main/java/com/example/recicla_mais/service/UsuarioService.java
@@ -0,0 +1,31 @@
+package com.example.recicla_mais.service;
+
+import com.example.recicla_mais.domain.Usuario;
+import com.example.recicla_mais.repository.UsuarioRepository;
+import com.example.recicla_mais.repository.entity.UsuarioEntity;
+import com.example.recicla_mais.service.mapper.UsuarioMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+    private final UsuarioRepository repository;
+
+    public Usuario cadastrar(Usuario usuario){
+        return UsuarioMapper.paraDomain(
+                repository.save(UsuarioMapper.paraEntity(usuario)));
+    }
+
+    public Usuario consultarPorId(Long id){
+        Optional<UsuarioEntity> usuarioConsultado = repository.findById(id);
+
+        if (usuarioConsultado.isEmpty()){
+            throw new RuntimeException("Usuário não encontrado");
+        }
+
+        return UsuarioMapper.paraDomain(usuarioConsultado.get());
+    }
+}

d57433caf4426664ad9111f9346a22eaea9b38f6
VitorVieira <vitorhvvieira@gmail.com>
2025-05-24 23:03:34 -0300

    Adicionando configurações de cors

---
+++ src/main/java/com/example/recicla_mais/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.example.recicla_mais.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
